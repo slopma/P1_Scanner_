54089c77b5596b01e80f34702b819280b474ea73
Paula Llanos <166711062+kirbchy@users.noreply.github.com>
2025-03-11 22:30:18 -0500

    Update main.dart

--- flutter_hu7/lib/main.dart
+++ flutter_hu7/lib/main.dart
@@ -5,8 +5,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize( //
-    url: 'https://zpprbzujtziokfyyhlfa.supabase.co', 
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwcHJienVqdHppb2tmeXlobGZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA3ODAyNzgsImV4cCI6MjA1NjM1NjI3OH0.cVRK3Ffrkjk7M4peHsiPPpv_cmXwpX859Ii49hohSLk', 
+    url: '...', 
+    anonKey: '...', 
   );
 
   runApp(MyApp());
